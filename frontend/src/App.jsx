bb0579a2f60a50c20d8f385c2225632c1606b2f6
Lui5-99 <luis_bravo_99@hotmail.com>
2023-09-14 20:26:24 -0600

    Frontend init whit Vite JS

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div className='flex justify-center items-center h-screen bg-white dark:bg-zinc-900'>
+      <h1 className='text-4xl font-bold text-black dark:text-white'>
+        Project Manager
+      </h1>
+    </div>
+  )
+}
+
+export default App
